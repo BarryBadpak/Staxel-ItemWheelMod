c2d65dbce9002bcd2b3cb6c014cefbe9da26d38d
Stefan Haneveer <s.haneveer93@gmail.com>
2018-02-06 23:56:43 +0100

    Make sure that with a custom UIScale the icons are rendered in it's correct location

---
+++ ClassicItemWheelMod/Patches/Weboverlayrenderer/TestAndUpdateUIScalePatch.cs
@@ -0,0 +1,19 @@
+﻿using Harmony;
+using Staxel;
+using Staxel.Client;
+using Staxel.Logic;
+using Staxel.Rendering;
+
+
+namespace ClassicItemWheelMod.Patches.Weboverlayrenderer
+{
+    [HarmonyPatch(typeof(WebOverlayRenderer), "TestAndUpdateUIScale")]
+    class TestAndUpdateUIScalePatch
+    {
+        [HarmonyPostfix]
+        static void afterTestAndUpdateUIScale()
+        {
+            ClientContext.WebOverlayRenderer.Call("rebindHotbar", null, null, null, null, null, null);
+        }
+    }
+}
